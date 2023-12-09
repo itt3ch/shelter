c27cc87b0172cd0630339d4e004597cd3f3812be
kandikq <kandikez@gmail.com>
2023-12-09 21:44:51 +0200

    Add all

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
+
 @app.route('/')
-def index():
+def main_page():
     return render_template('index.html')
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
+@app.route('/catalog')
+def catalog():
+    return render_template('catalog.html')
 
 
-if __name__ == "__main__":
-    app.run(debug = True)
+if __name__ == '__main__':
+    app.run(debug=True)
+
